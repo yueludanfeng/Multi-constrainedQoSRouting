e094d3b9994ec4c58518ee83024b040c1a8df50f
yueludanfeng <yueludanfeng@gmail.com>
2016-11-29 15:01:43 +0800

    Test Files

--- GUI/demo09.py
+++ GUI/demo09.py
@@ -11,3 +11,27 @@ print 'how are you doing!'
 print 'I am fine '
 print 'I add a new line'
 print 'hello'
+print datetime.datetime.now()
+a = [1,2,3,]
+print a[1:]
+import numpy as np
+print np.math.exp(3) * np.math.log10(10+2)
+
+class Demo:
+    demo =  2
+    def __init__(self):
+        pass
+
+demo = Demo()
+print Demo.demo
+print demo.demo
+print '--------------'
+for node in range(10):
+    print node
+
+s=[]
+t = -1
+s.append(1)
+s.append(2)
+print s.pop()
+print s.pop()
