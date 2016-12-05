27ae21740ef37f9cf35164ae59c8ae7ddc1790f6
yueludanfeng <yueludanfeng@gmail.com>
2016-12-05 16:15:03 +0800

    重构GlobalInfo 将test03.txt中ant_num由50改为20

--- ACO/ACO.py
+++ ACO/ACO.py
@@ -103,6 +103,16 @@ class GlobalInfo:
     pheromone = None
     delta_pheromone = None
 
+    @staticmethod
+    def init_param(node_num, edge_num, src_node, dst_node, ant_num, delay_w):
+        GlobalInfo.node_num = node_num
+        GlobalInfo.edge_num = edge_num
+        GlobalInfo.src_node = src_node
+        GlobalInfo.dst_node = dst_node
+        GlobalInfo.ant_num = ant_num
+        GlobalInfo.delay_w = delay_w
+
+
     def __init__(self):
         # 全局信息素矩阵初始`化
         GlobalInfo.pheromone = [[GlobalInfo.C for col in range(GlobalInfo.node_num)] for row in range(GlobalInfo.node_num)]
@@ -161,7 +171,10 @@ class Ant:
             print 'fm_sum = ', fm_sum
         # 如果分母为0，则说明当前结点没有相邻结点,需要回溯
         if fm_sum == 0:
+            print 'fm_sum==0'
+            # 表示没有找到下一个结点，而且赋值之后，要直接令返回，否则后面的代码会继续执行
             self.next_city = -1
+            return
 
         for city_index in self.allowed:
             # 在允许访问的结点中只需要考虑相邻结点（城市)
@@ -196,8 +209,12 @@ class Ant:
     def move_to_next_city(self):
         # 没找到下一个结点
         if self.next_city == -1:
-            self.Stack.pop()
-            self.current_city = self.Stack.pop()
+            self.solution.pop()
+            top = self.Stack.pop()
+            if top == self.current_city:
+                self.current_city = self.Stack.pop()
+            else:
+                self.current_city = top
         # 找到下一个结点
         else:
             self.current_city = self.next_city
@@ -205,6 +222,7 @@ class Ant:
             # self.solution.append(self.current_city)
             self.allowed.remove(self.current_city)
             self.tabu.append(self.current_city)
+            self.solution.append(self.current_city)
             self.Stack.append(self.current_city)
     # 局部信息素的更新，只更新该蚂蚁走过的路径上的信息素
     # 是每走一步更新一次，还是等走完之后在更新信息素呢
@@ -225,9 +243,24 @@ class Ant:
             self.move_to_next_city()
             if self.current_city == GlobalInfo.dst_node:
                 break
+            else:
+                print 'current_city=',self.current_city
         # 万万没想到,Stack就是解
-        self.solution = self.Stack
+        # self.solution = self.Stack
         self.update_pheromone()
+        print 'self.solution = ',self.solution
+
+
+class Population:
+    def __init__(self, ):
+        self.ant_num = GlobalInfo.ant_num
+        self.ants = [Ant(GlobalInfo.src_node, obj_graph) for num in range(self.ant_num)]
+
+    def solve(self):
+        for ant in self.ants:
+            ant.solve()
+
+
 
 if __name__ == "__main__":
     # 读取文件中相关信息
@@ -246,13 +279,8 @@ if __name__ == "__main__":
     print edge_num
     print ant_num
     print delay_w
-    GlobalInfo.node_num = node_num
-    GlobalInfo.edge_num = edge_num
-    GlobalInfo.src_node = src_node
-    GlobalInfo.dst_node = dst_node
-    GlobalInfo.ant_num = ant_num
-    GlobalInfo.delay_w = delay_w
     GlobalInfo()
+    GlobalInfo.init_param(node_num, edge_num, src_node, dst_node, ant_num, delay_w)
     obj_graph = Graph(node_num, edge_num)
     param_length = 5
     obj_graph.init_edge_measure(fp, param_length)
@@ -262,9 +290,9 @@ if __name__ == "__main__":
     print '------------------->graph.cost<------------'
     print obj_graph.cost
 
-    ant = Ant(GlobalInfo.src_node, obj_graph)
-    print "ant.solution=", ant.solution
-    ant.choose_next_city()
-    print "next_city = ",ant.next_city
+    population = Population()
+    population.solve()
+
+
 
 
